71bcdecf49afb43a1d95f079f5033e2d73527de7
SevenOne71 <wtyang2002@163.com>
2017-02-02 22:17:07 +0800

    Moq

---
+++ MiniDemo/MiniDemo/UnitTests/ShareLib/MoqTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTests.XmlParser
+{
+    public interface IFake
+    {
+        bool DoSomething(string val);
+        int ConventInt(int val);
+    }
+
+    [TestClass]
+    public class MoqTest
+    {
+        [TestMethod]
+        public void DoSomething_Test()
+        {
+            //refer to: http://www.cnblogs.com/techborther/archive/2012/01/10/2317998.html
+            var moq = new Mock<IFake>();
+            moq.Setup(s => s.DoSomething("ping")).Returns(true);
+            Assert.AreEqual(true, moq.Object.DoSomething("ping"));
+        }
+    }
+}
